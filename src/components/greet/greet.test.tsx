431d78e90a80c3cfa066a39d6d6708778bedf5b2
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-03-31 06:14:57 +0100

    first test

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -3,6 +3,9 @@
 write test
 writing code
 refactor
+
+test files with -test and -spec suffix
+also files in __tests__ folder
 */
 
 import { render, screen } from '@testing-library/react'
@@ -10,16 +13,19 @@ import Greet from './greet'
 
 // describe.only, describe.skip
 describe('Greet', () => {
-  test('renders correctly', () => {
+  // fit == test.only, xit == test.skip
+  it('renders correctly', () => {
     render(<Greet />)
     const textElement = screen.getByText(/hello/i)
     expect(textElement).toBeInTheDocument()
   })
-
-  // test.only, test.skip
-  test('renders with a  name', () => {
-    render(<Greet name="Chloie" />)
-    const textElement = screen.getByText('Hello Chloie')
-    expect(textElement).toBeInTheDocument()
-  })
 })
+
+// test.only, test.skip
+// describe('Nested', () => {
+//   test('renders with a name', () => {
+//     render(<Greet name="Chloie" />)
+//     const textElement = screen.getByText('Hello Chloie')
+//     expect(textElement).toBeInTheDocument()
+//   })
+// })
